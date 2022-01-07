ce6b4172a0243d9ce1182c0af2712f361c408431
Sylvainyabre <sylvainyabre7@gmail.com>
2022-01-07 13:32:43 -0800

    fixing color problem on phones

--- src/components/Navbar/MobileNav.js
+++ src/components/Navbar/MobileNav.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { GrClose } from "react-icons/gr";
 import { GiHamburgerMenu } from "react-icons/gi";
 import "./Navbar.css";
+import "../../index.css";
 
 
 
@@ -13,22 +14,22 @@ const MobileNav = () => {
   const mobileLinks = (
     <nav className="nav-menu">
        <GrClose className="close-icon" onClick={() => setOpen(!open)} />
-      <ul className="mobile-links">
+      <ul className="mobile-links justify-center">
        
 
-        <li className="mobile-link-item" onClick={() => setOpen(!open)}>
-          <Link to="/" className="mobile-link">
+        <li className="mobile-link-item center" onClick={() => setOpen(!open)}>
+          <Link to="/" className="mobile-link center">
             Home
           </Link>
         </li>
-        <li className="mobile-link-item" onClick={() => setOpen(!open)}>
-          <Link to="/about" className="mobile-link">
+        <li className="mobile-link-item text-center" onClick={() => setOpen(!open)}>
+          <Link to="/about" className="mobile-link text-center">
             About
           </Link>
         </li>
-        <li className="mobile-link-item" onClick={() => setOpen(!open)}>
+        <li className="mobile-link-item text-center" onClick={() => setOpen(!open)}>
           <div>
-            <Link to="/projects" className="mobile-link">Blog</Link>
+            <Link to="/projects" className="mobile-link text-center">Blog</Link>
           </div>
         </li>
       </ul>
